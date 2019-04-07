b3f0d4b9acacf6d56b4b09be637fd3db5b719b84
wmichaelbischoff <wmichaelbischoff@gmail.com>
2019-04-06 19:52:47 -0600

    done

--- Components/Wizard/Wizard.js
+++ Components/Wizard/Wizard.js
@@ -1,38 +1,117 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View , TextInput} from 'react-native';
-
-
+import { StyleSheet, Text, View , TextInput, TouchableOpacity} from 'react-native';
+import axios from 'axios'
+import {url} from '../../url'
 
 class Wizard extends Component{
     constructor(){
         super()
         this.state = {
-            header: null,
             name: '',
             address: '',
             city: '',
             state: '',
-            zipcode: ''
+            zip: '',
+            img: '',
+            mortgage: '',
+            rent: '',
+            stepOne: true,
+            stepTwo: false,
+            stepThree: false
         }
     }
     static navigationOptions = {
         title: 'Add New Property'
     }
+    postHouse(){
+        const { name, address, city, state, zip, img, mortgage, rent } = this.state;
+        axios.post(`${url.url}/api/house`, {name, address, city, state, zip, img, mortgage, rent}).then( () => {
+            this.setState({
+                name: '',
+                address: '',
+                city: '',
+                state: '',
+                zip: '',
+                img: '',
+                mortgage: '',
+                rent: '',
+                stepOne: true,
+                stepTwo: false,
+                stepThree: false
+            })
+            this.props.navigation.navigate('Home')
 
+        })
+    }
+    stepTwo = () => {
+        this.setState({
+            stepOne: false,
+            stepTwo: true,
+            stepThree: false,
+        })
+    }
+    stepThree = () => {
+        this.setState({
+            stepOne: false,
+            stepTwo: false,
+            StepThree: true
+        })
+    }
+    cancel = () => {
+        this.props.navigation.navigate('Home')
+        this.setState({
+            name: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: '',
+            img: '',
+            mortgage: '',
+            rent: '',
+            stepOne: true,
+            stepTwo: false,
+            stepThree: false
+        })
+    }
     render(){
+        if(this.state.stepOne === true){
         return(
+            
             <View style={styles.container}>
+                <TouchableOpacity style={styles.buttonTwo} onPress={() => {this.cancel()}}><Text>Cancel</Text></TouchableOpacity>
                 
                 <Text>Add New Listing</Text>
               
-                <TextInput value={this.state.name} placeholder="Property Name" onChangeText={(name) => this.setState({name})}/>
-                <TextInput value={this.state.address} placeholder="Address" onChangeText={(address) => this.setState({address})}/>
-                <TextInput value={this.state.city} placeholder="Name" onChangeText={(city) => this.setState({city})}/>
-                <TextInput value={this.state.state} placeholder="Name" onChangeText={(state) => this.setState({state})}/>
-                <TextInput value={this.state.zipcode} placeholder="Name" onChangeText={(zipcode) => this.setState({zipcode})}/>
-               
+                <TextInput style={styles.input} value={this.state.name} placeholder = "Property Name" placeholderTextColor ="black" onChangeText={(name) => this.setState({name})}/>
+                <TextInput style={styles.input} value={this.state.address} placeholder="Address" placeholderTextColor ="black" onChangeText={(address) => this.setState({address})}/>
+                <TextInput style={styles.input} value={this.state.city} placeholder="city" placeholderTextColor ="black" onChangeText={(city) => this.setState({city})}/>
+                <TextInput style={styles.input} value={this.state.state} placeholder="state" placeholderTextColor ="black" onChangeText={(state) => this.setState({state})}/>
+                <TextInput style={styles.input} value={this.state.zip} placeholder="zip" placeholderTextColor ="black" onChangeText={(zip) => this.setState({zip})}/>
+                <TouchableOpacity style={styles.button} onPress={() => { this.stepTwo()}}><Text>Next</Text></TouchableOpacity>
             </View>
         )
+        } else if(this.state.stepTwo === true){
+            return(
+                <View style={styles.container}>
+                <TouchableOpacity style={styles.buttonTwo} onPress={() => {this.cancel()}}><Text>Cancel</Text></TouchableOpacity>
+                
+                <Text>Add New Listing</Text>
+                <TextInput style={styles.input} value={this.state.img} placeholder="Image URL" placeholderTextColor ="black" onChangeText={(img) => this.setState({img})}/>
+                <TouchableOpacity style={styles.button} onPress={() => { this.stepThree()}}><Text>Next</Text></TouchableOpacity>
+            </View>
+            )
+        } else {
+            return (
+                <View style={styles.container}>
+                <TouchableOpacity style={styles.buttonTwo} onPress={() => {this.cancel()}}><Text>Cancel</Text></TouchableOpacity>
+                
+                <Text>Add New Listing</Text>
+                <TextInput style={styles.input} value={this.state.mortgage} placeholder="Mortgage" placeholderTextColor ="black" onChangeText={(mortgage) => this.setState({mortgage})}/>
+                <TextInput style={styles.input} value={this.state.rent} placeholder="Rent" placeholderTextColor ="black" onChangeText={(rent) => this.setState({rent})}/>
+                <TouchableOpacity style={styles.button} onPress={() => { this.postHouse()}}><Text>Add Home</Text></TouchableOpacity>
+            </View>
+            )
+        }
     }
 }
 
@@ -43,6 +122,23 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+      backgroundColor: '#AFD5C0',
+    },
+    button: {
+    alignItems: 'center',
+    backgroundColor: '#89EB91',
+    padding: 10
+    },
+    input: {
+       margin: 5,
+       height: 40,
+       width: 160,
+       borderColor: 'black',
+       borderWidth: 1
+    },
+    buttonTwo: {
+    alignItems: 'center',
+    backgroundColor: '#FEC2C2',
+    padding: 10
     }
   });
